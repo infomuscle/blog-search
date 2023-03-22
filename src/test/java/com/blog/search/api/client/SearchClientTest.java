7f7fc3c83257a73fabd25c51e6de750b075ebd2a
infomuscle <infomuscle10@gmail.com>
2023-03-22 15:42:16 +0900

    단위 테스트 케이스 추가

--- src/test/java/com/blog/search/api/client/SearchClientTest.java
+++ src/test/java/com/blog/search/api/client/SearchClientTest.java
@@ -4,6 +4,7 @@ import com.blog.search.api.client.external.ExternalClientAdapter;
 import com.blog.search.api.client.external.ExternalFeignClient;
 import com.blog.search.api.client.external.kakao.KakaoClientAdapter;
 import com.blog.search.api.client.external.kakao.KakaoFeignClient;
+import com.blog.search.api.client.external.kakao.message.KakaoClientResponse;
 import com.blog.search.api.client.external.naver.NaverClientAdapter;
 import com.blog.search.api.client.external.naver.NaverFeignClient;
 import com.blog.search.api.client.external.naver.message.NaverClientResponse;
@@ -66,20 +67,46 @@ class SearchClientTest {
         adapters.add(naverClientAdapter);
 
         ReflectionTestUtils.setField(searchClient, "clients", clients);
-
     }
 
-    @Test
-    @DisplayName("[실패] 카카오, 네이버 모두 실패")
-    public void testFailAll() {
+    @ParameterizedTest
+    @CsvSource({"10"})
+    @DisplayName("[성공] 카카오 성공")
+    public void testSuccessKakao(Integer size) {
 
         // given
+        List<KakaoClientResponse.Document> documents = new ArrayList<>();
+        for (int i = 0; i < size; i++) {
+            KakaoClientResponse.Document document = new KakaoClientResponse.Document();
+            ReflectionTestUtils.setField(document, "title", "title" + i);
+            ReflectionTestUtils.setField(document, "contents", "contents" + i);
+            ReflectionTestUtils.setField(document, "url", "url" + i);
+            ReflectionTestUtils.setField(document, "datetime", LocalDate.parse("20230322", DateTimeFormatter.BASIC_ISO_DATE).atStartOfDay());
+            documents.add(document);
+        }
+
+        KakaoClientResponse.Meta meta = new KakaoClientResponse.Meta();
+        ReflectionTestUtils.setField(meta, "totalCount", size);
+
+        KakaoClientResponse kakaoClientResponse = new KakaoClientResponse();
+        ReflectionTestUtils.setField(kakaoClientResponse, "documents", documents);
+        ReflectionTestUtils.setField(kakaoClientResponse, "meta", meta);
+
+        Mockito.when(kakaoClientAdapter.supports(kakaoFeignClient)).thenReturn(true);
+        Mockito.when(kakaoClientAdapter.search(any(), any(), any(), any(), any())).thenReturn(kakaoClientResponse);
 
         // when
-        AbstractThrowableAssert<?, ? extends Throwable> throwableAssert = assertThatThrownBy(() -> searchClient.search("jpa", "accuracy", 1, 10));
+        SearchClientResponse searchClientResponse = searchClient.search("jpa", "accuracy", 1, size);
 
         // then
-        throwableAssert.isInstanceOf(SearchBusinessException.class).hasMessage(ApiResult.외부_API_통신_실패.getMessage());
+        assertThat(searchClientResponse.getTotalCount()).isEqualTo(size);
+        for (int i = 0; i < searchClientResponse.getPosts().size(); i++) {
+            SearchClientResponse.Post post = searchClientResponse.getPosts().get(i);
+            assertThat(post.getTitle()).isEqualTo("title" + i);
+            assertThat(post.getContents()).isEqualTo("contents" + i);
+            assertThat(post.getUrl()).isEqualTo("url" + i);
+            assertThat(post.getPostDate()).isEqualTo(LocalDate.parse("20230322", DateTimeFormatter.BASIC_ISO_DATE));
+        }
     }
 
     @ParameterizedTest
@@ -102,7 +129,7 @@ class SearchClientTest {
         ReflectionTestUtils.setField(naverClientResponse, "total", size);
         ReflectionTestUtils.setField(naverClientResponse, "items", items);
 
-        Mockito.when(naverClientAdapter.supports(any())).thenReturn(true);
+        Mockito.when(naverClientAdapter.supports(naverFeignClient)).thenReturn(true);
         Mockito.when(naverClientAdapter.search(any(), any(), any(), any(), any())).thenReturn(naverClientResponse);
 
         // when
@@ -117,5 +144,18 @@ class SearchClientTest {
             assertThat(post.getUrl()).isEqualTo("link" + i);
             assertThat(post.getPostDate()).isEqualTo(LocalDate.parse("20230322", DateTimeFormatter.BASIC_ISO_DATE));
         }
+    }
+
+    @Test
+    @DisplayName("[실패] 카카오, 네이버 모두 실패")
+    public void testFailAll() {
+
+        // given
+
+        // when
+        AbstractThrowableAssert<?, ? extends Throwable> throwableAssert = assertThatThrownBy(() -> searchClient.search("jpa", "accuracy", 1, 10));
+
+        // then
+        throwableAssert.isInstanceOf(SearchBusinessException.class).hasMessage(ApiResult.외부_API_통신_실패.getMessage());
     }
 }
